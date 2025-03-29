6aaad4549902ffca14ebcf2653753c80474be697
EspirituCastilloKevin <166048982+Espiritu16@users.noreply.github.com>
2025-03-28 21:27:39 -0500

    Add files via upload

---
+++ src/diseñodepatrones/MostradorDeEstadisticas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package diseñodepatrones;
+
+/**
+ *
+ * @author danie
+ */
+public interface MostradorDeEstadisticas {
+
+    void mostrarEstadisticas(); //Mostrara las estadisticas
+
+}
