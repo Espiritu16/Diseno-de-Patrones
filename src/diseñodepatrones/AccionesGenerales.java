bcf6cbc2e040bf63e970ecb606b571ed0a9a898f
FTP2305 <felixtintaya2305@email.com>
2025-03-28 20:34:04 -0500

    FINALIZAR SEM1

--- src/diseñodepatrones/AccionesGenerales.java
+++
@@ -1,8 +0,0 @@
-package diseñodepatrones;
-public interface  AccionesGenerales {
-    // void ingresarDatos(); //ingreso de la cantidad y tipo de registro
-    void mostrarResiduos();   //mostrara los residuos generados por los usuarios
-    void mostrarSugerencia(); //mostrara una sugerencia de metodos de reciclaje o reutilizacion
-    void mostrarInformacion(); //mostrara un recordatorio para la recolleccion de residuos
-    void mostrarEstadisticas(); //mostrara el consumo y reciclaje de manera estadistica
-}
